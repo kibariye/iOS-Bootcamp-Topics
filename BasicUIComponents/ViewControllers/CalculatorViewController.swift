9b78f0b235dbd0078389e1e08a23fc69bf8cadc8
kibariye <kibariyeguney@gmail.com>
2021-09-15 22:31:48 +0300

    Kibariye Guney Hafta3

--- BasicUIComponents/ViewControllers/CalculatorViewController.swift
+++ BasicUIComponents/ViewControllers/CalculatorViewController.swift
@@ -11,6 +11,11 @@ class CalculatorViewController: UIViewController {
 
     @IBOutlet weak var resultLabel: UILabel!
     
+    var screenNumber : Double = 0
+    var previousNumber : Double = 0
+    var mathOperation=false
+    var operation=0
+    
     //@IBOutlet var digitButtons: [UIButton]!
     
     override func viewDidLoad() {
@@ -24,24 +29,92 @@ class CalculatorViewController: UIViewController {
     @IBAction func digitButtonTapped(_ sender: UIButton) {
         let digit = String(sender.tag)
         let displayText = resultLabel.text ?? ""
-
         
         if isTyping {
-            resultLabel.text = displayText + digit
+            if mathOperation {
+                resultLabel.text = digit
+                mathOperation = false
+            }
+            else {
+                resultLabel.text = displayText + digit
+            }
+            
         } else {
             resultLabel.text = digit
             isTyping.toggle()
         }
+        screenNumber = Double(resultLabel.text!)!
+    }
+    
+    class OperationCalculation {
+        let opr :Int
+        let previousNum, numberOnScr: Double
+            
+        init(opr:Int, previousNum : Double, numberOnScr : Double) {
+            self.opr = opr
+            self.previousNum = previousNum
+            self.numberOnScr = numberOnScr
+        }
+        
+        func makeCalculation() ->String{
+            switch opr {
+            case 11:
+                return String(previousNum * numberOnScr)
+            case 12:
+                return String(previousNum - numberOnScr)
+            case 13:
+                return String(previousNum + numberOnScr)
+            case 14:
+                return String(previousNum / numberOnScr)
+            default:
+                return ""
+            }
+        }
     }
     
     @IBAction func actionButtonTapped(_ sender: UIButton) {
-        
+                
+        let digit = sender.tag
         let value = NSString(string: resultLabel.text ?? "0").doubleValue
-        resultLabel.text = String(sqrt(value))
+        
+        if resultLabel.text != "" && sender.tag != 15 && sender.tag != 16 {
+
+            previousNumber = value
+            
+            switch digit {
+            case 10:
+                resultLabel.text = String(sqrt(value))
+            case 11:
+                resultLabel.text = "x";
+            case 12:
+                resultLabel.text = "-";
+            case 13:
+                resultLabel.text = "+";
+            case 14:
+                resultLabel.text = "/";
+            default:
+                break
+            }
+            
+           operation = sender.tag
+
+           mathOperation = true;
+        }
+        else if sender.tag == 15{
+        
+            let operationCalculation = OperationCalculation(opr:operation, previousNum : previousNumber, numberOnScr : screenNumber)
+            
+            resultLabel.text = operationCalculation.makeCalculation()
+            
+        } else if sender.tag == 16{
+
+            resultLabel.text = "0"
+            previousNumber = 0;
+            screenNumber = 0;
+            operation = 0;
+            isTyping.toggle()
+        }
     }
-    
-    
-    
     
     /*
     // MARK: - Navigation
